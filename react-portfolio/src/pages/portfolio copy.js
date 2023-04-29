c2b4fb43a7caf8401be6296821b93470350464f6
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2023-04-29 18:02:50 -0500

    Added the ability to download my resume

--- react-portfolio/src/pages/portfolio copy.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Resume = () => {
-return (
-	<h1>Add Resume!</h1>
-);
-};
-
-export default Resume;
--- react-portfolio/src/pages/resume.js
+++ react-portfolio/src/pages/resume.js
@@ -1,8 +1,31 @@
 import React from 'react';
 
+// Function will execute on click of button
+const onButtonClick = () => {
+	// using Java Script method to get PDF file
+	fetch('SDV_Resume.pdf').then(response => {
+		response.blob().then(blob => {
+			// Creating new object of PDF file
+			const fileURL = window.URL.createObjectURL(blob);
+			// Setting various property values
+			let alink = document.createElement('a');
+			alink.href = fileURL;
+			alink.download = 'SDV_Resume.pdf';
+			alink.click();
+		})
+	})
+}
+
 const Resume = () => {
 return (
-	<h1>Add Resume!</h1>
+	<div>	
+		<h3>Resume</h3>
+		<h3>Click on below button to download PDF file</h3>
+				<button onClick={onButtonClick}>
+					Download PDF
+				</button>
+	</div>
+
 );
 };
 
