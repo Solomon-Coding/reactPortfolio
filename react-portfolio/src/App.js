e3a1477dc2d62f02df5b31462f9d092b5a5345c2
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2023-04-29 08:42:04 -0500

    Added the code for the bucket list app to make sure everything was working

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import './App.css';
+import BucketList from './components/BucketList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bucket-app">
+      <BucketList />
     </div>
   );
 }
